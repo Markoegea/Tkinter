49e899a1a344c04fcb9915db7a78400076d91b20
Marco Alejandro Egea Moreno <markoegea@gmail.com>
2021-06-14 18:56:07 -0500

    El marco 2

--- ExplorarTtinker.py
+++ ExplorarTtinker.py
@@ -2,6 +2,6 @@ from tkinter import *
 ventana=Tk()
 ventana.title('funciona!!')
 marco=Frame(ventana)
-marco.config(bg='red',width=400, height=400)
+marco.config(bg='pink',width=400, height=400)
 marco.pack()
 ventana.mainloop()
