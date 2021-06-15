980d580acb7ef8d78b44ee39f9c7e457ef177109
Marco Alejandro Egea Moreno <markoegea@gmail.com>
2021-06-14 19:18:28 -0500

    El Combobox

--- ExplorarTtinker.py
+++ ExplorarTtinker.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from tkinter.ttk import *
 ventana=Tk()
 ventana.title('funciona!!')
-entrada=Entry(ventana)
-entrada.config(justify='center')
-entrada.pack()
+combo=Combobox(ventana)
+combo['values']=(1,2,3,4,5, 'Texto',9.8)
+combo.current(0)
+combo.grid(column=0, row=0)
+combo.pack()
 ventana.mainloop()
