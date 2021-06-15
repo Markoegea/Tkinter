58f86bc844d783658c51be959aba0406e7cd60d9
Marco Alejandro Egea Moreno <markoegea@gmail.com>
2021-06-14 19:29:20 -0500

    Proto Checkbox

--- ExplorarTtinker.py
+++ ExplorarTtinker.py
@@ -2,16 +2,10 @@ from tkinter import *
 from tkinter.ttk import *
 ventana=Tk()
 ventana.title('funciona!!')
-combo=Combobox(ventana)
-combo['values']=('HOMBRE', 'MUJER', 'Texto',9.8)
-combo.current(0)
-combo.grid(column=0, row=0)
-combo.pack()
 
-def accion():
-    if combo.current() == 0:
-        combo.current(1)
+def verificar():
+    pass
 
-boton=Button(ventana, text='Mostar',command=accion)
-boton.pack()
+checkeo=IntVar()
+
 ventana.mainloop()
