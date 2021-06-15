c76590e230154d3ed5e350ea51ca3bb8b05a2bc8
Marco Alejandro Egea Moreno <markoegea@gmail.com>
2021-06-14 20:11:05 -0500

    El ScrolledText

--- ExplorarTtinker.py
+++ ExplorarTtinker.py
@@ -1,20 +1,10 @@
 from tkinter import *
 from tkinter.ttk import *
-
+from tkinter import scrolledtext
 
 ventana=Tk()
 ventana.title('funciona!!')
 
-def seleccionar():
-    valor=sexo.get()
-    if (valor == 1):
-        print('Femenino')
-    else:
-        print('Masculino')
-
-sexo=IntVar()
-rdSexo1= Radiobutton(ventana, text='Masculino', variable=sexo, value=0, command=seleccionar)
-rdSexo2= Radiobutton(ventana, text='Femenino', variable=sexo, value=1, command=seleccionar)
-rdSexo1.pack()
-rdSexo2.pack()
+textolargo=scrolledtext.ScrolledText(ventana, width=20, height=10)
+textolargo.grid(column=3, row=0)
 ventana.mainloop()
